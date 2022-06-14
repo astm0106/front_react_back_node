101f9e486122641953379a5202ca096870fd6e12
astm0106 <astm0106@gmail.com>
2022-06-14 21:47:42 +0900

    board

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,52 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 const pool = require('./pool.js');
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// urlencodedとjsonは別々に初期化する
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.get("/api", async function (req, res, next) {
-  console.log("=============================");
-
-  // 取得前
-  console.log('取得前 totalCount: ', pool.totalCount);
-  console.log('取得前 idleCount: ', pool.idleCount);
-  console.log('取得前 waitingCount: ', pool.waitingCount);
-
   // コネクション取得
   const connect = await pool.connect();
-
-  // 取得後
-  console.log('取得後 totalCount: ', pool.totalCount);
-  console.log('取得後 idleCount: ', pool.idleCount);
-  console.log('取得後 waitingCount: ', pool.waitingCount);
-
   // クエリ発行
   const result = await connect.query('SELECT MAX(CAST(id AS INTEGER)) AS maxid FROM kenko_record');
-
   const ans = result.rows[0].maxid + 1;
-
   const queryInsert = {
     text: 'INSERT INTO kenko_record (id, name, food) VALUES ($1, $2, $3)',
     values: [ans, 'kevin', 'banana'],
-}
+  }
   await connect.query(queryInsert);
+  // コネクション返却
+  connect.release();
+  //console.log(ans);
+  res.json({ message: ans });
+});
+
+app.post("/api2", async function (req, res, next) {
+  // コネクション取得
+  const connect = await pool.connect();
+  // クエリ発行
+  const queryInsert = {
+    text: 'INSERT INTO msg_board (message, posting_time) VALUES ($1, NOW())',
+    values: [req.body.message],
+  }
+  await connect.query(queryInsert);
+  const result = await connect.query(
+    'SELECT message, to_char(posting_time, \'YYYY/MM/DD HH24:MI:SS\') AS posting_time FROM msg_board ORDER BY posting_time DESC LIMIT 50'
+    );
+  //console.log(result);
 
   // コネクション返却
   connect.release();
-
-  // 返却後
-  console.log('返却後 totalCount: ', pool.totalCount);
-  console.log('返却後 idleCount: ', pool.idleCount);
-  console.log('返却後 waitingCount: ', pool.waitingCount);
-
-  console.log(ans);
-
-  res.json({ message: ans});
-
+  res.json(result);
 });
 
 app.get('*', (req, res) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,77 @@
 import './App.css';
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() =>{
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  },[])
+  const [message, setMessage] = useState('ボタンを押してください');
+  const [message2, setMessage2] = useState('');
+  const [result, setResult] = useState(
+    [
+      {
+        message: "",
+        posting_time: ""
+      }
+    ]
+  );
+
+  //useEffect(() => addMsg(), [])
   return (
     <div className="App">
       <h1>フロントエンド</h1>
-      <p>{ message }</p>
+      <p>{message}</p>
+      <button onClick={() => addMsg()}>接続</button>
+      <form class="fetchForm">
+        <textarea
+          placeholder='メッセージを入力してください'
+          rows="4"
+          cols="40"
+          onChange={(e) => setMessage2(e.target.value)}
+          value={message2}
+        />
+      </form>
+      <button onClick={() => addMsg2()}>メッセージ送信</button>
+      {result.map((str) =>
+        <div>
+          <p>{str.message}</p>
+          {str.posting_time}
+        </div>
+      )}
     </div>
   );
+
+  function addMsg() {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }
+
+  function addMsg2() {
+    if (message2 !== "") {
+      
+      const jsonData = {
+        message: message2
+      }
+      setMessage2("");
+
+      fetch('/api2',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(jsonData)
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const array = [];
+          for (const elem of data.rows) {
+            array.push(elem);
+          }
+          setResult(array);
+        }
+        );
+    }
+  }
 }
 
 export default App;
